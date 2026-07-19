6c3e84ddb1e671c2a588f7bc551541cb80d5fc6a
Djenadi Mohammed <Djenadimohammed22@gmail.com>
2026-07-19 18:15:47 +0200

    removing duplicates and trimming company names

--- SQL/data-cleaning/Datacleaning.sql
+++ SQL/data-cleaning/Datacleaning.sql
@@ -1,4 +1,4 @@
-
+SET SQL_SAFE_UPDATES = 0;
 #world_layoffs was imported as csv to table layoffs uder the database world_layoffs
 
 select * from layoffs;
@@ -25,7 +25,32 @@ insert into layoffs_staging
 select * from layoffs;
 
 
+#Removing Duplicates 
 
+CREATE TABLE `layoffs_staging_2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `rn` int 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+insert into layoffs_staging_2
+	select *, (row_number() over(partition by company,location,`date`,stage, percentage_laid_off, funds_raised_millions)) as rn
+    from layoffs_staging ;
+
+delete from layoffs_staging_2 where rn > 1 ;
+
+#--Standarizing the data
+
+update layoffs_staging_2 
+set company = trim(company);
 
 
 
